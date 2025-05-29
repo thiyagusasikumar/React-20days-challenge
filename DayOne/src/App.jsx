176724e1fae7b0e3823126cf59ca0d0f2415513d
Thiyagu S <sthiyagusasikumark@gmail.com>
2025-05-29 23:34:21 +0530

    day two task

--- DayOne/src/App.jsx
+++ DayOne/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Flower from './assets/img.png'
 import { Navbar } from "./components/Navbar"; // Import the Navbar component correctly
 let x="Learning React";
 let array=["user_1","user_2","user_3","user_4"];
 let data="boy";
-function listen(){
+function Listen(){
   console.log("Entered");
-    
+  return <img src={Flower} alt="hi" style={{ height: "300px", width: "300px" }} />
 }
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
       }
       {data==="girl"?<h1>BOY</h1>:<h1>GIRL</h1>}
       <h1>Event Handlers</h1>
-      <button onClick={listen}>Click Me</button>
+      <Listen /> 
     </div>
   );
 };
